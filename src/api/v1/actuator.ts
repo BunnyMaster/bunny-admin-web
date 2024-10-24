96def30fab8bf9bc1b42ff2b659b3687acbd0651
Bunny <1319900154@qq.com>
2024-10-24 23:37:50 +0800

    fix: 🧩 修复路径缺陷

--- src/api/v1/actuator.ts
+++ src/api/v1/actuator.ts
@@ -2,15 +2,15 @@ import { http } from '@/api/service/request';
 
 /** actuator断端点-系统服务获取 */
 export const fetchSystemHealthList = () => {
-	return http.request<any>('get', '/health');
+	return http.request<any>('get', 'actuator/health');
 };
 
 /** actuator断端点-系统信息 */
 export const fetchSystemInfo = () => {
-	return http.request<any>('get', '/info');
+	return http.request<any>('get', 'actuator/info');
 };
 
 /** actuator断端点-系统缓存 */
 export const fetchSystemCaches = () => {
-	return http.request<any>('get', '/caches');
+	return http.request<any>('get', 'actuator/caches');
 };
