ae4323c18ba1470e76c3517436a7ed9180cbb1ab
Bunny <1319900154@qq.com>
2024-10-30 22:04:31 +0800

    optimize: ♻️ 多语言连表查询显示用户名

--- src/views/i18n/i18n-setting/utils/columns.tsx
+++ src/views/i18n/i18n-setting/utils/columns.tsx
@@ -11,8 +11,8 @@ export const columns: TableColumnList = [
 	{ label: $t('i18n.typeName'), prop: 'typeName' },
 	{ label: $t('table.updateTime'), prop: 'updateTime', sortable: true },
 	{ label: $t('table.createTime'), prop: 'createTime', sortable: true },
-	{ label: $t('table.createUser'), prop: 'createUser', slot: 'createUser', width: 90 },
-	{ label: $t('table.updateUser'), prop: 'updateUser', slot: 'updateUser', width: 90 },
+	{ label: $t('table.createUser'), prop: 'createUser', slot: 'createUser', width: 130 },
+	{ label: $t('table.updateUser'), prop: 'updateUser', slot: 'updateUser', width: 130 },
 	{ label: $t('table.operation'), fixed: 'right', width: 210, slot: 'operation' },
 ];
 
--- src/views/i18n/i18n-type-setting/utils/columns.tsx
+++ src/views/i18n/i18n-type-setting/utils/columns.tsx
@@ -10,8 +10,8 @@ export const columns: TableColumnList = [
 	{ label: $t('isDefault'), prop: 'isDefault', slot: 'isDefault' },
 	{ label: $t('table.updateTime'), prop: 'updateTime' },
 	{ label: $t('table.createTime'), prop: 'createTime' },
-	{ label: $t('table.createUser'), prop: 'createUser', slot: 'createUser', width: 90 },
-	{ label: $t('table.updateUser'), prop: 'updateUser', slot: 'updateUser', width: 90 },
+	{ label: $t('table.createUser'), prop: 'createUser', slot: 'createUser', width: 130 },
+	{ label: $t('table.updateUser'), prop: 'updateUser', slot: 'updateUser', width: 130 },
 	{ label: $t('table.operation'), fixed: 'right', width: 210, slot: 'operation' },
 ];
 
